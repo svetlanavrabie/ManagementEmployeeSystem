b0f131a105f9d0a3f03c141ba36c011efc191f0d
Svetlana Vrabie <laurasdamir@gmail.com>
2024-07-04 23:32:40 -0400

    refactor: setup the migration file to create the database in  sql server;

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using ServerLibrary.Data;
+using ServerLibrary.Helpers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// starting
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")
+           ?? throw new InvalidOperationException("Sorry, your connection is not found"));
+});
+
+builder.Services.Configure<JwtSection>(builder.Configuration.GetSection("JwtSection"));
 
 var app = builder.Build();
 
---
+++ ServerLibrary/Repositories/Implementations/UserAccountRepository.cs
@@ -0,0 +1,19 @@
+﻿using BaseLibrary.Dtos;
+using BaseLibrary.Responses;
+using ServerLibrary.Repositories.Contracts;
+
+namespace ServerLibrary.Repositories.Implementations
+{
+    public class UserAccountRepository : IUserAccount
+    {
+        public Task<GeneralResponse> CreateAsync(Register user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponse> SignInAsync(Login user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
