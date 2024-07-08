f2d29ecdced869aba65983dfdcff2a129f1e090d
Svetlana Vrabie <laurasdamir@gmail.com>
2024-07-07 21:41:16 -0400

    refactor: small fix;

--- ServerLibrary/Repositories/Implementations/UserAccountRepository.cs
+++ ServerLibrary/Repositories/Implementations/UserAccountRepository.cs
@@ -1,19 +1,55 @@
 ﻿using BaseLibrary.Dtos;
+using BaseLibrary.Entities;
 using BaseLibrary.Responses;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using ServerLibrary.Data;
+using ServerLibrary.Helpers;
 using ServerLibrary.Repositories.Contracts;
 
 namespace ServerLibrary.Repositories.Implementations
 {
-    public class UserAccountRepository : IUserAccount
+    public class UserAccountRepository(IOptions<JwtSection> config, AppDbContext dbContext) : IUserAccount
     {
-        public Task<GeneralResponse> CreateAsync(Register user)
+        public async Task<GeneralResponse> CreateAsync(Register user)
         {
-            throw new NotImplementedException();
+            if (user is null)
+            {
+                return new GeneralResponse(false, "Model is empty");
+            }
+
+            var checkUser = await FindUserByEmailAsync(user.Email!);
+
+            if (checkUser != null)
+            {
+                return new GeneralResponse(false, "User registered already");
+            }
+
+            var appUser = await AddToDatabase(new ApplicationUser()
+            {
+                FullName = user.FullName,
+                Email = user.Email,
+                Password = BCrypt.Net.BCrypt.HashPassword(user.Password)
+            });
+
+            return new GeneralResponse(true, string.Empty);
         }
 
         public Task<LoginResponse> SignInAsync(Login user)
         {
             throw new NotImplementedException();
+        }
+
+        private async Task<ApplicationUser> FindUserByEmailAsync(string email)
+        {
+            return await dbContext.ApplicationUsers.FirstOrDefaultAsync(u => u.Email!.Equals(email!, StringComparison.OrdinalIgnoreCase));
+        }
+
+        private async Task<T> AddToDatabase<T>(T model)
+        {
+            var result = dbContext.Add(model!);
+            await dbContext.SaveChangesAsync();
+            return (T)result.Entity;
         }
     }
 }
